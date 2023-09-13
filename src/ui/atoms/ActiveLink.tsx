d433920e74cf005e20059c201304ffd3d50c681d
annakijanka <ankazrob@gmail.com>
2023-09-14 00:25:44 +0200

    fixes for Link

--- src/ui/atoms/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink.tsx
@@ -10,10 +10,10 @@ export const ActiveLink = ({ href, children }: { href: string; children: React.R
 	return (
 		<Link
 			className={clsx(
-				"flex h-full w-full min-w-[3rem] items-center justify-center border-b-2 border-transparent px-1 pt-1 text-center text-sm font-medium text-slate-500 hover:border-gray-300 hover:text-slate-700",
-				{ "border-blue-500": isActive },
+				"flex h-full w-full min-w-[3rem] items-center justify-center border-b-2 px-1 pt-1 text-center text-sm font-medium text-slate-500 hover:border-gray-300 hover:text-slate-700",
+				{ "border-transparent": !isActive, "border-blue-500": isActive },
 			)}
-			href={href}
+			href={{ pathname: href }}
 		>
 			{children}
 		</Link>

f7c8021c8c480aee88b0d2474c2f89dc245eb321
annakijanka <ankazrob@gmail.com>
2023-09-17 17:58:59 +0200

    changes to ActiveLink

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 							<nav className="-mx-2 flex overflow-x-scroll lg:mx-0 lg:h-16 lg:overflow-x-auto">
 								<ul className="flex h-16 max-w-full space-x-8 whitespace-nowrap lg:px-8">
 									<li className="first:pl-4 last:pr-4 lg:px-0">
-										<ActiveLink href="/">Home</ActiveLink>
+										<ActiveLink
+											className="border-transparent"
+											activeClassName="border-blue-500"
+											href="/"
+											exact={true}
+										>
+											Home
+										</ActiveLink>
 									</li>
 									<li className="first:pl-4 last:pr-4 lg:px-0">
-										<ActiveLink href="/products">Products</ActiveLink>
+										<ActiveLink
+											className="border-transparent"
+											activeClassName="border-blue-500"
+											href="/products"
+										>
+											All
+										</ActiveLink>
 									</li>
 								</ul>
 							</nav>
--- src/ui/atoms/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink.tsx
@@ -1,19 +1,39 @@
 "use client";
 
 import clsx from "clsx";
+import { type Route } from "next";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export const ActiveLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
+export const ActiveLink = <T extends Route>({
+	className,
+	activeClassName,
+	href,
+	exact = false,
+	children,
+}: {
+	className?: string;
+	activeClassName: string;
+	href: T;
+	exact?: boolean;
+	children: React.ReactNode;
+}) => {
 	const pathname = usePathname();
-	const isActive = pathname === href;
+	let isActive = false;
+
+	if (exact) {
+		isActive = pathname === href;
+	} else {
+		isActive = pathname.startsWith(href);
+	}
+
 	return (
 		<Link
 			className={clsx(
 				"flex h-full w-full min-w-[3rem] items-center justify-center border-b-2 px-1 pt-1 text-center text-sm font-medium text-slate-500 hover:border-gray-300 hover:text-slate-700",
-				{ "border-transparent": !isActive, "border-blue-500": isActive },
+				{ [className || ""]: !isActive, [activeClassName]: isActive },
 			)}
-			href={{ pathname: href }}
+			href={href}
 		>
 			{children}
 		</Link>

1149e69b7aeafefe05e63716e910c06f90d63902
annakijanka <ankazrob@gmail.com>
2023-10-02 21:23:42 +0200

    more subtle loader

--- src/ui/atoms/Loading.tsx
+++ src/ui/atoms/Loading.tsx
@@ -1,6 +1,6 @@
 export const Loading = () => {
 	return (
-		<div className="flex gap-1.5" aria-busy="true">
+		<div className="flex gap-1.5 opacity-25" aria-busy="true">
 			<div className="h-3 w-3 animate-pulse rounded-full bg-gun-powder"></div>
 			<div className="h-3 w-3 animate-pulse rounded-full bg-gun-powder"></div>
 			<div className="h-3 w-3 animate-pulse rounded-full bg-gun-powder"></div>

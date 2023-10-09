329886179c3487599692ea721fdfc124c372d6eb
annakijanka <ankazrob@gmail.com>
2023-10-09 21:42:07 +0200

    temporary sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,22 @@
+// To be refined in the future
+
+import { type MetadataRoute } from "next";
+
+export default function Sitemap(): MetadataRoute.Sitemap {
+	const appUrl = process.env.APP_URL;
+
+	if (!appUrl) {
+		throw new Error("APP_URL environment variable is not defined.");
+	}
+
+	return [
+		{
+			url: appUrl,
+			lastModified: new Date(),
+		},
+		{
+			url: `${appUrl}/products`,
+			lastModified: new Date(),
+		},
+	];
+}

6b4e6a7b962eaf7c2f068aef7a9d202b3fa94e81
annakijanka <ankazrob@gmail.com>
2023-10-11 16:22:16 +0200

    change for webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -4,9 +4,18 @@ import { type NextRequest } from "next/server";
 export async function POST(request: NextRequest): Promise<Response> {
 	const body: unknown = await request.json();
 
-	if (typeof body === "object" && body && "id" in body && typeof body.id === "string") {
-		console.log(`Revalidating /product/${body.id}`);
-		revalidatePath(`/product/${body.id}`);
+	if (
+		typeof body === "object" &&
+		body !== null &&
+		"data" in body &&
+		typeof body.data === "object" &&
+		body.data !== null &&
+		"id" in body.data &&
+		typeof body.data.id === "string"
+	) {
+		const productId: string = body.data.id;
+		console.log(`Revalidating /product/${productId}`);
+		revalidatePath(`/product/${productId}`);
 		console.log(`Revalidating /products`);
 		revalidatePath(`/products`);
 		return new Response(null, { status: 204 });

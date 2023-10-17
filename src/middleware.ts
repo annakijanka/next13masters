055df7a961e8d573ab418c01194a30489585023e
annakijanka <ankazrob@gmail.com>
2023-10-17 10:30:38 +0200

    fix for paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,15 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
 	publicRoutes: [
 		/^\/$/,
-		/^\/privacy-policy\/$/,
-		/^\/return-policy\/$/,
-		/^\/terms-of-service\/$/,
-		/^\/cart\/.*$/,
-		/^\/categories\/.*$/,
-		/^\/collections\/.*$/,
-		/^\/product\/.*$/,
-		/^\/products\/.*$/,
-		/^\/search\/.*$/,
+		/^\/privacy-policy$/,
+		/^\/return-policy$/,
+		/^\/terms-of-service$/,
+		/^\/cart(.*)$/,
+		/^\/categories(.*)$/,
+		/^\/collections(.*)$/,
+		/^\/product(.*)$/,
+		/^\/products(.*)$/,
+		/^\/search(.*)$/,
 	],
 });
 
